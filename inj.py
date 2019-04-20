38f2c211d0f9de74be1108b3ad9a1ad192d9278c
Pan Zhang <panzhang0216@gmail.com>
2019-04-20 16:31:51 -0700

    New update!
    Now this code will print what you order and the time you ordered it in a
    file on your desktop

--- inj.py
+++ inj.py
@@ -1,3 +1,5 @@
+import datetime
+import calendar
 menu = {'espresso' : 3.00, 'americano' : 3.00, 'macchiato' : 3.50 , 'cortado' : 4.00 , 'cappuccino' : 4.00 , 'pour-over' :  5.00 , \
     'iced brew' : 4.00 , 'iced tea' :  3.00 , 'milk tea' :  5.00 , 'cupp joe' :  5.00 , 'chai latte' : {'8oz':  3.00, '12oz':  3.75, '16oz': 4.25} , \
     'matcha latte' : {'8oz': 3.00, '12oz': 3.75, '16oz': 4.25} , 'hot cocoa' : {'8oz' : 3.00, '12oz' :  3.50, '16oz' : 4.25}}
@@ -9,9 +11,11 @@ for m in menu.keys():
 #     print(menu.get(x).get(size))
 # else:
 #     print(menu.get(x))
+order_file = open('c:\\Users\\panzh\\Desktop\\orders.txt', 'w')
 def order_drinks():
     more = True
     total = []
+    order=[]
     while more == True:
         drink = input('What drink do you want?  ')
         if drink == 'chai latte' or drink == 'matcha latte':
@@ -21,6 +25,11 @@ def order_drinks():
                 pass
             else:
                 total.append(menu.get('chai latte').get(size))
+                # order_file.write('%s %s %s\n' % (str(datetime.datetime.now()), drink, size,))
+                t = datetime.datetime.now()
+                m = calendar.month_name[t.month]
+                order_file.write('%s %d %d %d %d %d %s %s\n' % (m, t.day, t.year, t.hour, t.minute, t.second, drink, size))
+                # order_file('%s %d %d \n' % (m, t.day, t.year))
         elif drink == 'hot cocoa':
             size = input('What size do you want?(8oz, 12oz, 16oz)  ')
             print(menu.get('hot cocoa').get(size))
@@ -28,12 +37,18 @@ def order_drinks():
                 pass
             else:
                 total.append(menu.get('hot cocoa').get(size))
+                t = datetime.datetime.now()
+                m = calendar.month_name[t.month]
+                order_file.write('%s %d %d %d %d %d %s %s\n' % (m, t.day, t.year, t.hour, t.minute, t.second, drink, size))
         else:
             print('The price for %s is %s' %(drink, menu.get(drink)))
             if menu.get(drink) == None:
                 pass
             else:
                 total.append(menu.get(drink))
+                t = datetime.datetime.now()
+                m = calendar.month_name[t.month]
+                order_file.write('%s %d %d %d %d %d %s\n' % (m, t.day, t.year, t.hour, t.minute, t.second, drink))
         w = input('Do you want more?  ')
         if w == 'Yes' or w == 'yes':
             pass
@@ -44,3 +59,4 @@ def order_drinks():
         else:
             print('I dont understand')
 order_drinks()
+order_file.close()
